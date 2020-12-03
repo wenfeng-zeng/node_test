63e6a8331e1565aa0667c2b346196fa7f6c7878a
zengwenfeng <Wf930522>
2020-12-03 17:43:56 +0800

    抽奖，抽奖历时

--- controller/controller.js
+++ controller/controller.js
@@ -5,15 +5,73 @@ module.exports = {
       if (err) console.error(err);
       let resObj = null;
       console.log(result)
-      if (result.affectedRows == 1) {
+      if (result) {
         resObj = {
           code: 200,
-          msg: 'success'
+          msg: 'success',
+          data: result
         }
       } else {
         resObj = {
           code: 401,
-          msg: 'fail'
+          msg: 'fail',
+          data: []
+        }
+      }
+      res.send(resObj)
+    })
+  },
+  getPrize (req, res) {
+    listModule.getPrize((err, result) => {
+      if (err) console.error(err);
+      let resObj = null;
+      if (result) {
+        resObj = {
+          code: 200,
+          msg: 'success',
+          // data:result
+        }
+        let randomNum = Math.ceil(Math.random() * 100)
+        try {
+          result.forEach(e => {
+            if (randomNum > e.number) {
+              randomNum -= e.number
+            } else {
+              resObj.data = e
+              listModule.prizeHistory(e, (err1, result1) => {
+                if (err1) console.error(err1);
+              })
+              throw ('循环终止')
+            }
+          });
+        } catch (e) {
+          console.log('e: ', e)
+        }
+      } else {
+        resObj = {
+          code: 401,
+          msg: 'fail',
+          data: []
+        }
+      }
+      res.send(resObj)
+    })
+  },
+  getPrizeHistory (req, res) {
+    listModule.getPrizeHistory((err, result) => {
+      if (err) console.error(err);
+      let resObj = null;
+      if (result) {
+        resObj = {
+          code: 200,
+          msg: 'success',
+          data: result
+        }
+      } else {
+        resObj = {
+          code: 401,
+          msg: 'fail',
+          data: []
         }
       }
       res.send(resObj)
--- module/module.js
+++ module/module.js
@@ -7,5 +7,27 @@ module.exports = {
     connection.sqlConnection.query(sql, (err, result) => {
       callback(err, result)
     })
-  }
+  },
+  getPrize (callback) {
+    // let sql = "update prizeList set name='京豆', number=15 where id=1";
+    // connection.connect();
+    let sql = 'SELECT * FROM prizeList';
+    connection.sqlConnection.query(sql, (err, result) => {
+      callback(err, result)
+    })
+  },
+  prizeHistory (val, callback) {
+    // let sql = `SELECT * FROM prizeList where id=${id}`;
+    let sql = `INSERT INTO prizeHistory (name, number, isdeleted, id) VALUES ('${val.name}', ${val.number}, ${val.isdelete}, ${val.id})`
+    connection.sqlConnection.query(sql, (err, result) => {
+      callback(err, result)
+    })
+  },
+  getPrizeHistory (callback) {
+    let sql = 'SELECT * FROM prizeHistory';
+    // connection.connect();
+    connection.sqlConnection.query(sql, (err, result) => {
+      callback(err, result)
+    })
+  },
 }
